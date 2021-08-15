2bb24ce1a606d3e25d4cd0cb8dd64ddba790a4cd
felixyunianto <felixyunianto@gmail.com>
2021-08-16 00:44:28 +0700

    Modify Hero and About

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,47 +1,62 @@
 import React from "react";
 import { useHistory, withRouter } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchSquadDetail } from '../../redux/actions/userActions'
+import { fetchSquadDetail } from "../../redux/actions/userActions";
 
 const Hero = () => {
   const squads = useSelector((state) => state.squadReducers.squads);
-  const history = useHistory()
+  const history = useHistory();
   const dispatch = useDispatch();
 
   const handleDetailSquad = (e) => {
-    dispatch(fetchSquadDetail(e.target.id, null))
+    dispatch(fetchSquadDetail(e.target.id, null));
     history.push({
-      pathname : '/about-squad',
-      state : {
-        squadId : e.target.id
-      }
-    })
-  }
+      pathname: "/about-squad",
+      state: {
+        squadId: e.target.id,
+      },
+    });
+  };
 
   return (
     <div>
       <section className="text-gray-600 body-font pt-36 sm:pt-28 md:pt-24 lg:pt-24">
         <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-3 font-bold text-gray-900">
-              PLUG-IN
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center lg:-mr-32 -mt-12">
+            <h1 className="title-font text-6xl mb-3 font-bold text-gray-900 tracking-widest">
+              Plugin
             </h1>
-            <nav className="flex flex-wrap items-center text-base justify-between mb-5 gap-3">
-              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">Passionate</p>
-              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">Learning</p>
-              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">Unity</p>
-              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">Global</p>
-              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">Inovate</p>
-              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">Nationality</p>
-            </nav>
-            <p className="mb-8 leading-relaxed text-2xl">
-              Komunitas linux dan pengembangan aplikasi di Politeknik Harapan
-              Bersama Tegal.
-            </p>
+            <div className="flex flex-wrap items-center text-lg font-medium justify-between mb-5 gap-3 w-full">
+              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">
+                Passionate
+              </p>
+              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">
+                Learning
+              </p>
+              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">
+                Unity
+              </p>
+              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">
+                Global
+              </p>
+              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">
+                Inovate
+              </p>
+              <p className="hover:text-gray-900 lg:block md:hidden sm:hidden">
+                Nationality
+              </p>
+            </div>
+
+            <div className="w-full">
+              <p className="mb-8 leading-relaxed text-3xl tracking-wide font-medium">
+                komunitas linux dan pengembangan aplikasi di Politeknik Harapan
+                Bersama Tegal.
+              </p>
+            </div>
           </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <div className="w-6/12">
             <img
-              className="object-cover object-center object-containt rounded"
+              className="object-cover object-center object-contain rounded h-full w-full"
               alt="hero"
               src="https://res.cloudinary.com/zada/image/upload/v1628083472/Hero-min_k4j4lu.png"
             />
@@ -49,29 +64,29 @@ const Hero = () => {
         </div>
       </section>
 
-      <section className="text-gray-600 body-font lg:-mt-36 sm:mt-20 md:mt-20">
-        <div className="container mx-auto flex md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-            <img
-              className="object-cover object-center rounded -ml-16"
-              alt="hero"
-              src="https://res.cloudinary.com/zada/image/upload/v1628083471/tentang-min_na5nuh.png"
-            />
-          </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              Tentang PLUG-IN
-            </h1>
-            <p className="mb-8 mx-auto leading-relaxed text-1xl text-base">
-              <span className="font-bold">PLUG-IN</span> adalah suatu organisasi
-              atau komunitas studi linux dan pengembangan aplikasi di Politeknik
-              Harapan Bersama Tegal. Kami mempunyai tujuan untuk mengenalkan dan
-              mengOpen Source kan mahasiswa dan masyarakat umum dengan berbagai
-              kegiatan yang nantinya bisa membuka wawasan serta mindset
-              OpenSource guna untuk mengurangi angka pembajakan Software di
-              Indonesia
+      <section className="text-gray-600 body-font lg:-mt-24 sm:mt-20 md:mt-20 flex">
+        <div className="w-5/12 left-0">
+          <img
+            className="object-cover"
+            alt="hero"
+            src="https://res.cloudinary.com/zada/image/upload/v1628083471/tentang-min_na5nuh.png"
+          />
+        </div>
+        <div className="w-8/12 p-32 flex flex-col items-start justify-center">
+          <h1 className="title-font text-5xl font-bold text-gray-900 tracking-wider">
+            Tentang Plugin
+          </h1>
+          <div className="w-auto my-3">
+            <p className="leading-relaxed text-xl text-base tracking-wide font-medium">
+              PLUG-IN adalah suatu organisasi atau komunitas studi linux dan
+              pengembangan aplikasi di Politeknik Harapan Bersama Tegal. Kami
+              mempunyai tujuan untuk mengenalkan dan mengOpen Source kan
+              mahasiswa dan masyarakat umum dengan berbagai kegiatan yang
+              nantinya bisa membuka wawasan serta mindset OpenSource guna untuk
+              mengurangi angka pembajakan Software di Indonesia
             </p>
           </div>
+          <button className="bg-biru self-center text-white p-4 py-2 rounded-full">Lihat selengkapnya</button>
         </div>
       </section>
 
@@ -79,7 +94,7 @@ const Hero = () => {
         <div className="container px-5 mx-auto py-10">
           <div className="flex flex-col text-center w-full mb-5">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-            PLUG-IN Squad
+              PLUG-IN Squad
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-1xl text-base">
               <span className="font-bold">Squad</span> di{" "}
@@ -93,11 +108,24 @@ const Hero = () => {
           <div className="flex flex-wrap -m-4">
             {squads.map((squad, index) => {
               return (
-                <div className="p-4 lg:w-1/2 md:w-full cursor-pointer" key={index} onClick={handleDetailSquad}>
-                  <div id={squad.id} className="border-2 rounded-lg hover:bg-biru hover:text-white">
-                    <div className="flex p-8 sm:flex-row flex-col text-center" id={squad.id}>
+                <div
+                  className="p-4 lg:w-1/2 md:w-full cursor-pointer"
+                  key={index}
+                  onClick={handleDetailSquad}
+                >
+                  <div
+                    id={squad.id}
+                    className="border-2 rounded-lg hover:bg-biru hover:text-white"
+                  >
+                    <div
+                      className="flex p-8 sm:flex-row flex-col text-center"
+                      id={squad.id}
+                    >
                       <div className="flex-grow">
-                        <h2 className="font-bold text-lg title-font font-medium mb-3"  id={squad.id}>
+                        <h2
+                          className="font-bold text-lg title-font font-medium mb-3"
+                          id={squad.id}
+                        >
                           {squad.squads_name}
                         </h2>
                         <p className="leading-relaxed text-base" id={squad.id}>
